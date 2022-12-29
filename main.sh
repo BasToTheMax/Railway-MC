e2f66425bf638fdcf3433f73ec4e6eb031134802
BasToTheMax <bas@arnold-linda.nl>
2022-12-29 17:35:34 +0100

    new jar

--- main.sh
+++
@@ -1,7 +0,0 @@
-# chmod u+x ./gotty
-# ./gotty bash
-
-echo "welcome"
-echo "bttm :)"
-
-bash mc.sh
--- mc.sh
+++ mc.sh
@@ -105,6 +105,4 @@ java -Xmx512M -Xms512M -jar server.jar --port 25565 --offline-mode
 # java -Xmx384M -Xms384M -jar server.jar nogui
 # java -jar server.jar
 
-bash ./start.sh
-
 echo "Exit code $?"
